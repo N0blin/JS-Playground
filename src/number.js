ee259473f508879ac9e0f3e6c2ed254841fc5153
n0blin <noah.eric.geiger@gmail.com>
2024-03-07 16:26:18 +1300

    added if else

--- src/number.js
+++ src/number.js
@@ -2,11 +2,51 @@
 /*
 LOL
 */
-const num1 = 10
-console.log(num1)
+// const pi = 3.14
+// //pi = 10
+// console.log(pi)
 
-let num2 = 20
-console.log(num2)
+// let num2 = 20
+// num2 = 25
+// console.log(num2)
 
-var num3 = 30
-console.log(num3)
+// var num3 = 30
+// console.log(num3)
+
+// const FullName = "Noah Geiger"
+// //console.log(FullName)
+
+// const workplace = 'NMIT Student'
+// //console.log(workplace)
+
+// let Age = '19'
+// //console.log(Age)
+
+// let Gender = 'M'
+// //console.log(Gender)
+// //console.log(FullName + " " + workplace + " " + Age)
+
+// console.log(`My full name is ${FullName} and i work at ${workplace} and i am ${Age} years old`)
+
+// const working = true
+// const notworking = false
+
+// if(working) {
+// console.log(FullName)
+// }
+
+// if (pi == 3.14) {
+//     console.log(FullName)
+// }
+
+let a = 2
+let result
+// if() {} else {}
+a = 0
+if (a >= 0) {
+    result = 'positive';
+    console.log(result)
+} else {
+    result = 'Not positive';
+    console.log(result)
+}
