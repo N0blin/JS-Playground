3d5f1d0ae39deea88c9927811fb1abf97b309a47
n0blin <noah.eric.geiger@gmail.com>
2024-03-14 14:44:59 +1300

    Rock Paper Scissor three types

--- src/functions.js
+++ src/functions.js
@@ -58,8 +58,49 @@
 
 // console.log(arrayByTwo([2,5,4]))
 // One line
-const oneLineArray = (arr) => arr.map(el => el * 2)
+//const oneLineArray = (arr) => arr.map(el => el * 2)
 
-console.log(oneLineArray([2,5,4]))
-console.log(oneLineArray([4,10,8]))
-console.log(oneLineArray([8,20,16]))
+// console.log(oneLineArray([2,5,4]))
+
+
+
+//Create a function which takes two strings (p1 and p2) as arguments and returns a string
+//stating the winner (Rock, Paper, Scissors)
+
+//NOTE - Game Condition
+// if p1 wins return the string "The winner is P1"
+// if p2 wins return the string "The winner is P2"
+// if they are the same then return "It's a draw"
+
+function rps(p1, p2) {
+    if(p1 === p2) return `It's a draw`
+
+    else if(p1 === 'Rock' && p2 === 'Scissors') return 'The winner is P1';
+    else if (p1 === 'Paper' && p2 === 'Rock') return 'The winner is P1';
+    else if(p1 === 'Scissors' && p2 === 'Paper') return 'The winner is P1';
+
+    else return 'The winner is P2'
+}
+
+// console.log(rps('Rock', 'Scissors'))
+
+
+//ANCHOR - ES6 JavaScript built in functions.
+function rpsTwo(p1, p2) {
+
+    let obj = {
+        Rock: 'Scissors',
+        Scissors: 'Paper',
+        Paper: 'Rock'
+    }
+    return p1 === p2 ? "Its a draw" : obj[p1] === p2 ? " The winner is p1" : "The winner is p2"
+}
+
+//ANCHOR - JS built in function inculdes()
+function rps(p1, p2) {
+    const wins = ['RockScissors', 'PaperRock', 'ScissorPaper']
+/*
+
+*/
+    return p1 === p2 ? 'Its a draw' : `The winner is ${wins.includes(p1 + p2)} ? 'p1' : 'p2'`
+}
